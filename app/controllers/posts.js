c7f9dbd20111943a334077a7fb480e0cad759739
libybbh <liyaberidze1827@gmail.com>
2020-10-29 04:08:04 +0200

    Change old property function to computed in controller

--- app/controllers/posts.js
+++ app/controllers/posts.js
@@ -1,7 +1,12 @@
 import Controller from '@ember/controller';
+import { computed } from '@ember/object';
 
 export default Controller.extend({
-  reverse: function(){
+  /*reverse: function(){
+    return this.get('model').toArray().reverse(); old  version of property=computed
+  }.property('model.[]')*/
+  reverse: computed('model.[]', function () {
     return this.get('model').toArray().reverse();
-  }.property('model.[]')
+  })
 });
+
