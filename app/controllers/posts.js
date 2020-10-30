8fd5679391450e97772cc770e0ada19805ab85f7
libybbh <liyaberidze1827@gmail.com>
2020-10-30 02:46:10 +0200

    button is disabled when userId, title and/or body are empty

--- app/controllers/posts.js
+++ app/controllers/posts.js
@@ -1,12 +1,31 @@
 import Controller from '@ember/controller';
-import { computed } from '@ember/object';
+import {isPresent} from '@ember/utils';
+import {computed, observer, set} from '@ember/object';
 
 export default Controller.extend({
-  /*reverse: function(){
-    return this.get('model').toArray().reverse(); old  version of property=computed
-  }.property('model.[]')*/
+  isDisabled: true,
+
   reverse: computed('model.[]', function () {
     return this.get('model').toArray().reverse();
+  }),
+
+  isBtnDisabled: observer('userId', 'title', 'body', function () {                  // how it observe correct variables?
+    if (!isPresent(this.userId) | !isPresent(this.title) | !isPresent(this.body)) { // why here we can't use userId body title like in line above
+      set(this, 'isDisabled', true);
+    } else set(this, 'isDisabled', false);
   })
 });
 
+/*
+  // old computed method
+  reverse: function(){
+    return this.get('model').toArray().reverse();
+  }.property('model.[]')
+
+  // old observer method
+  valueObserver: function () {
+    if (this.n_userId === null | !isPresent(this.n_title) | !isPresent(this.n_body)) {
+      set(this, 'isDisabled', true);
+    } else set(this, 'isDisabled', false);
+  }.observes('n_title', 'n_body', 'n_userId')
+*/
--- app/routes/posts.js
+++ app/routes/posts.js
@@ -2,7 +2,7 @@ import Route from '@ember/routing/route';
 import $ from 'jquery';
 
 export default Route.extend({
-  model (){
+  model() {
     return this.store.findAll('post');
   },
 
@@ -11,44 +11,46 @@ export default Route.extend({
       var myStore = this.store;
       let jsonData = $.getJSON('https://jsonplaceholder.typicode.com/posts');
 
-      if(n_userId >= 1 & n_userId <= 100)
-      {
-        jsonData.then(function(data) {  // can get data only from function(data)
-        var sizeOfData = data.length;
-        alert("JsonData array size is: " + sizeOfData);
+      if (n_userId >= 1 & n_userId <= 100) {
+        jsonData.then(function (data) {  // can get data only from function(data)
+          var sizeOfData = data.length;
+          alert("JsonData array size is: " + sizeOfData);
 
-        var newPost={
-          userId: n_userId,
-          id: sizeOfData + 1,
-          title: n_title,
-          body: n_body
-        }
-
-        myStore.pushPayload({
-          posts: [newPost]
-        });
-      });
-      }else alert('Put correct UserID!');
-
-     //let jsonData = $.getJSON('https://jsonplaceholder.typicode.com/posts');
-     //var size1 = Object.keys(jsonData).length; -> get 16, not 100
-
-      /*var newRecord = this.store.push('post', {
-        userId: p_userId,
-        title: p_title,
-        body: p_body
-      });
-      ---------------
-      this.store.pushPayload({
-        posts: [
-          {
-            userId: p_userId,
-            id: this.store.key(),
-            title: p_title,
-            body: p_body
+          var newPost = {
+            userId: n_userId,
+            id: sizeOfData + 1,
+            title: n_title,
+            body: n_body
           }
-        ]
-      });*/
 
-    }}
+          myStore.pushPayload({
+            posts: [newPost]
+          });
+        });
+      } else alert("Put correct ID!");
+    }
+  }
 });
+
+/*
+
+// return promise, not an array
+     let jsonData = $.getJSON('https://jsonplaceholder.typicode.com/posts');
+     var size1 = Object.keys(jsonData).length; -> get 16, not 100
+
+// old pushes variants
+     var newRecord = this.store.push('post', {
+      userId: p_userId,
+      title: p_title,
+      body: p_body
+     });
+      ---------------
+     this.store.pushPayload({
+      posts: [{
+       userId: p_userId,
+       id: this.store.key(),
+       title: p_title,
+       body: p_body
+      }]
+     });
+*/
